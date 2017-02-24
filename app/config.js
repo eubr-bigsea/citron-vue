976196f3be38e3d17906bac7426e8e9733c242c1
Walter dos Santos Filho <waltersf@gmail.com>
2017-02-24 17:58:38 -0300

    Separation of configuration

---
+++ app/config.js
@@ -0,0 +1,12 @@
+const tahitiUrl = 'http://beta.ctweb.inweb.org.br/tahiti';
+//const tahitiUrl = 'http://localhost:5000'; 
+
+//const standUrl = 'http://beta.ctweb.inweb.org.br/stand';
+const standUrl = 'http://localhost:3323';
+
+//const limoneroUrl = 'http://localhost:5000';
+const limoneroUrl = 'http://beta.ctweb.inweb.org.br/limonero';
+
+const authToken = '123456';
+
+export { tahitiUrl, standUrl, limoneroUrl, authToken };
