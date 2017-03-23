252554a1e1d8bfa58b690e03370dde0afe724031
Walter dos Santos Filho <waltersf@gmail.com>
2017-03-23 19:38:22 -0300

    Added data source list

---
+++ app/components/app/data-source-list.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="container">
+        <div class="row small-padding">
+            <div class="col-md-12">
+                <h2>Data Sources</h2>
+            </div>
+            <hr/>
+            <div class="col-md-12">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th class="sortable primary text-center col-md-1" @click="sort('id')">
+                                 Id <span class="fa" :class="['fa-' + (this.asc === 'false' ? 'sort-up': 'sort-down')]" v-show="orderBy === 'id'"></span>
+                                 </th>
+                            <th class="sortable primary text-center col-md-1" @click="sort('name')">
+                                 Name <span class="fa" :class="['fa-' + (this.asc === 'false' ? 'sort-up': 'sort-down')]" v-show="orderBy === 'id'"></span>
+                                 </th>
+                            <th class="sortable primary text-center col-md-2" @click="sort('created')">
+                                Created <span class="fa" :class="['fa-' + (this.asc === 'false' ? 'sort-up': 'sort-down')]" v-show="orderBy === 'updated'"></span>
+                                </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="ds in dataSources">
+                            <td class="text-center">
+                                <router-link :to="{name: 'data-source-detail', params: {id: ds.id }}">{{ds.id}}</router-link>
+                            </td>
+                            <td class="text-center">{{ds.name}}</td>
+                            <td class="text-center">{{ formatDate(ds.created, 'DD-MM-YYYY HH:mm:ss') }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p class="text-center">Showing {{pageData.pagination.page}} of {{pageData.pagination.pages}} pages</p>
+                <nav>
+                    <ul class="pager">
+                        <li>
+                            <router-link :to="{name: 'data-source-page', params: {page: page - 1 }, query:{sort: orderBy, asc: asc }}" @click.native="paginate" v-show="pageData.pagination.page !== 1">Previous</router-link>
+                        </li>
+                        <li>
+                            <router-link :to="{name: 'data-source-page', params: {page: page + 1}, query:{sort: orderBy, asc: asc }}" @click.native="paginate" v-show="pageData.pagination.page !== pageData.pagination.pages">Next</router-link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import ListingMixin from '../mixins/listing-mixin';
+    import MomentMixin from '../mixins/moment-mixin';
+    import {
+        limoneroUrl
+    } from '../../config';
+
+    const DataSourceListComponent = Vue.extend({
+        data() {
+            return {
+                dataSources: []
+            };
+        },
+        /* Life-cycle */
+        mounted: function() {
+            this.performLoad();
+        },
+        /* Methods */
+        methods: {
+            getData(data) {
+                //data['platform'] = this.platform;
+            },
+            paginate(ev) {
+                ev.stopPropagation();
+                return false;
+            },
+            load(parameters) {
+                let self = this;
+                this.$store.dispatch('updatePageParameters', {
+                    page: 'data-source-list',
+                    parameters
+                });
+                let url = `${limoneroUrl}/datasources?enabled=true`;
+                let headers = {
+                    'X-Auth-Token': '123456'
+                }
+
+                Vue.http.get(url, {
+                    parameters,
+                    headers
+                }).then(response => {
+                    self.dataSources = response.data;
+                });
+            },
+            performLoad(reload, orderBy) {
+                let saved = this.$store.getters.getPageParameters['workflow-list'];
+                if (this.$route.params.page || !saved || reload) {
+                    if (!reload) {
+                        this.page = parseInt(this.$route.params.page) || 1;
+                    } else {
+                        this.page = 1;
+                    }
+                    if (orderBy) {
+                        this.asc = (this.asc === 'true') ? 'false' : 'true';
+                    }
+                    let data = {
+                        page: this.page,
+                        size: 20,
+                        platform: this.platform,
+                        sort: orderBy || '',
+                        asc: this.asc,
+                    }
+                    this.load(data);
+                } else {
+                    this.page = saved.page;
+                    this.platform = saved.platform || 'spark';
+                    this.load(saved);
+                }
+            }
+        },
+        mixins: [ListingMixin, MomentMixin],
+        /* Events */
+        watch: {
+            '$route': function() {
+                if (this.$route.params.page) {
+                    this.page = parseInt(this.$route.params.page);
+                    let data = {
+                        fields: this.fields,
+                        page: this.page,
+                        size: 20,
+                        platform: this.platform
+                    };
+                    this.load(data);
+                }
+            }
+        }
+    });
+    export default DataSourceListComponent;
+</script>
--- app/main.js
+++ app/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+
 import AppComponent from './components/app/app';
 import WorkflowListComponent from './components/app/workflow-list.vue';
 import JobListComponent from './components/app/job-list.vue';
+import DataSourceListComponent from './components/app/data-source-list.vue';
+
 import JobDetailComponent from './components/app/job-detail.vue';
 import WorkflowAddComponent from './components/app/workflow-add.vue';
 import LoginComponent from './components/app/login.vue';
@@ -41,10 +44,10 @@ Vue.http.interceptors.push((request, next) => {
         Vue.prototype.$Progress.increase(100);
     }*/
     document.querySelector('.page-overlay').style.visibility = 'visible';
-    requestCounter ++;
+    requestCounter++;
     next((response) => {
-        requestCounter --;
-        if (requestCounter === 0){
+        requestCounter--;
+        if (requestCounter === 0) {
             //Vue.prototype.$Progress.finish();
             document.querySelector('.page-overlay').style.visibility = 'hidden';
         }
@@ -59,31 +62,84 @@ const Foo = { template: '<div>foo</div>' }
 const Dashboard = { template: '<div>Dashboard Page!!!!</div>' }
 
 /* Routes configuration */
-const routes = [
-    { path: '/workflows/add', component: WorkflowAddComponent, name: 'add-workflow',
-        meta: {title: 'New workflow' }},
-    { path: '/workflows/editor/:id', component: AppComponent, name: 'editor',
-    meta: {title: 'Edit workflow' }},
-    { path: '/workflows/list/:page', component: WorkflowListComponent, name: 'workflow-page',
-    meta: { title: 'Workflow list' }},
-    { path: '/workflows/list', component: WorkflowListComponent, name: 'workflow-list',
-    meta: {title: 'Workflow list'}},
+const routes = [{
+        path: '/workflows/add',
+        component: WorkflowAddComponent,
+        name: 'add-workflow',
+        meta: { title: 'New workflow' }
+    },
+    {
+        path: '/workflows/editor/:id',
+        component: AppComponent,
+        name: 'editor',
+        meta: { title: 'Edit workflow' }
+    },
+    {
+        path: '/workflows/list/:page',
+        component: WorkflowListComponent,
+        name: 'workflow-page',
+        meta: { title: 'Workflow list' }
+    },
+    {
+        path: '/workflows/list',
+        component: WorkflowListComponent,
+        name: 'workflow-list',
+        meta: { title: 'Workflow list' }
+    },
 
-    { path: '/jobs/list/:page', component: JobListComponent, name: 'job-page',
-    query: {sort: 'id'}, meta: {title: 'Job list'}},
-    { path: '/jobs/list', component: JobListComponent, name: 'job-list',
-    meta: {title: 'Job list' }},
-    { path: '/jobs/:id', component: JobDetailComponent, name: 'job-detail',
-    meta: {title: 'Job result' }},
+    {
+        path: '/jobs/list/:page',
+        component: JobListComponent,
+        name: 'job-page',
+        query: { sort: 'id' },
+        meta: { title: 'Job list' }
+    },
+    {
+        path: '/jobs/list',
+        component: JobListComponent,
+        name: 'job-list',
+        meta: { title: 'Job list' }
+    },
+    {
+        path: '/jobs/:id',
+        component: JobDetailComponent,
+        name: 'job-detail',
+        meta: { title: 'Job result' }
+    },
 
 
-    { path: '/login', component: LoginComponent, name: 'login',
-    meta: {title: 'Login' }},
-    { path: '/dashboard', component: Dashboard, name: 'dashboard',
-    meta: {title: 'Dashboard' }},
+    {
+        path: '/login',
+        component: LoginComponent,
+        name: 'login',
+        meta: { title: 'Login' }
+    },
+    {
+        path: '/dashboard',
+        component: Dashboard,
+        name: 'dashboard',
+        meta: { title: 'Dashboard' }
+    },
 
-    { path: '/data-sources/list', component: Foo,
-    meta: {title: 'Data sources' }},
+    {
+        path: '/data-source/list',
+        name: 'data-source-list',
+        component: DataSourceListComponent,
+        meta: { title: 'Data sources' }
+    },
+    {
+        path: '/data-source/:id',
+        component: DataSourceListComponent,
+        name: 'data-source-detail',
+        meta: { title: 'Data sources' }
+    },
+    {
+        path: '/data-source/list/:page',
+        component: DataSourceListComponent,
+        name: 'data-source-page',
+        query: { sort: 'id' },
+        meta: { title: 'Data sources' }
+    },
     { path: '*', redirect: '/workflows/list' }
 ]
 
@@ -91,13 +147,13 @@ const router = new VueRouter({
     routes,
     mode: 'hash',
 });
-router.beforeEach(function (to, from, next) {
+router.beforeEach(function(to, from, next) {
     eventHub.$emit('route-change', to, from);
-    if (to.meta.title){
+    if (to.meta.title) {
         document.title = to.meta.title;
     }
     next();
-    return 
+    return
     let auth = { state: { token: null } };
     if (to.name !== 'login' || true) {
         if (!auth.state.token) {
@@ -126,7 +182,7 @@ new Vue({
     store,
 }).$mount('#app');
 
-Vue.config.errorHandler = function (err, vm) {
+Vue.config.errorHandler = function(err, vm) {
     console.debug('Erro no VUE', err, vm);
     return false;
 }
