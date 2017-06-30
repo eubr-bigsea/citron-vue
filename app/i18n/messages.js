50d4d88a5918eb60d4b85299a7f40f4418bb3c99
Walter dos Santos Filho <waltersf@gmail.com>
2017-06-30 18:24:26 -0300

    I18n messages

---
+++ app/i18n/messages.js
@@ -0,0 +1,65 @@
+const citron_messages = {
+  en: {
+    actions: {
+        save: 'Save'
+    },
+    common: {
+        action: 'Action | Actions',
+        attribute: 'Attribute | Attributes',
+        created: 'Created',
+        description: 'Description',
+        enabled: 'Enabled',
+        format: 'Format',
+        id: 'Id',
+        name: 'Name | Names',
+        nullable: 'Nullable',
+        owner: 'Owner',
+        permission: 'Permission | Permissions',
+        precision: 'Precision',
+        scale: 'Scale',
+        size: 'Size',
+        type: 'Type | Types',
+        userId: 'User id',
+        userLogin: 'User login',
+        userName: 'User name',
+    },
+    titles: {
+        dataSource: 'Data source | Data sources',
+    },
+    dataSourceDetail: {
+      noPermissions: 'No permissions defined',
+      noAttributes: 'No attributes defined',
+      missingRepresentation: 'Missing representation'
+    }
+  },
+  pt: {
+    actions: {
+        save: 'Salvar'
+    },
+    common: {
+        attribute: 'Atributo | Atributos',
+        description: 'Descrição',
+        enabled: 'Habilitado',
+        format: 'Formato',
+        name: 'Nome | Nomes',
+        nullable: 'Aceita nulo',
+        precision: 'Precisão',
+        scale: 'Escala',
+        size: 'Tamanho',
+        type: 'Tipo | Tipo',
+        userId: 'Id de usuário',
+        userLogin: 'Login',
+        userName: 'Nome do usuário',
+    },
+    titles: {
+        dataSource: 'Fonte de dados | Fontes de dados',
+    },
+    dataSourceDetail: {
+      permission: 'Permissão | Permissões',
+      noPermissions: 'Nenhuma permissão definida',
+      noAttributes: 'Nenhum atributo definida',
+      missingRepresentation: 'Representação para ausentes'
+    }
+  }
+}
+export default citron_messages;
